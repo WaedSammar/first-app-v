f41341623555998631c131f40366716e5370c661
Waed Sammar <waedsammar12@gmail.com>
2024-12-20 19:51:57 +0200

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,68 @@
+import { useState } from "react";
 import "./App.css";
 import Student from "./component/student/student.component";
+import { IStudent } from "./types";
 
-const COURSES_LIST = ['React', 'HTML', 'CSS'];
+
+const COURSES_LIST: string[] = ["React", "HTML", "CSS"];
+const INITIAL_LIST: Array<IStudent> = [
+  {
+    id: '2401',
+    name: "Waad Amer",
+    age: 20,
+    isGraduated: false,
+    coursesList: ['Math', 'English'],
+  },
+  {
+    id: '2402',
+    name: "Seba Rabee",
+    age: 18,
+    isGraduated: false,
+    coursesList: ['Web Dev', 'Science', 'React', 'HTML', 'Science'],
+  },
+  {
+    id: '2403',
+    name: "Sayyaf Sammar",
+    age: 24,
+    isGraduated: true,
+    coursesList: COURSES_LIST,
+  },
+  {
+    id: '2404',
+    name: "Abood Mohammad",
+    age: 15,
+    isGraduated: false,
+    coursesList: COURSES_LIST,
+  },
+];
 
 function App() {
+  const [studentsList, setStudentsList] = useState<IStudent[]>(INITIAL_LIST);
+
+  const removeFirst = () => {
+    const newList = [...studentsList];
+    newList.shift();
+    // newList.pop();  Remove Last student
+    setStudentsList(newList);
+  }
   return (
     <div>
       <h1>Welcome to GSG React/Next Course</h1>
-      <Student name="Rand Sammar" age={22} isGraduated={true} coursesList={COURSES_LIST} />
-      <Student name="Seba Rabee" age={18} isGraduated={false} coursesList={COURSES_LIST} />
-      <Student name="Sayyaf Hassan" age={24} isGraduated={true} coursesList={COURSES_LIST} />
-      <Student name="Abood Mohammad" age={15} isGraduated={false} coursesList={COURSES_LIST} />
+      <button onClick={removeFirst}>Remove First Student</button>
+      {
+        studentsList.map(student => (
+          <Student
+            id={student.id}
+            key={student.id}
+            name={student.name}
+            age={student.age}
+            isGraduated={student.isGraduated}
+            coursesList={student.coursesList}
+          />
+        ))
+      }
     </div>
-  );
+  )
 }
 
 export default App;
--- src/component/student/student.component.tsx
+++ src/component/student/student.component.tsx
@@ -1,25 +1,55 @@
+import { useState } from "react";
+import { IStudent } from "../../types";
 import CoursesList from "../courses-list/courses-list.component";
 import "./student.css";
 
-interface IProps {
-  name: string;
-  age: number;
-  isGraduated: boolean;
-  coursesList: string[];
+interface IProps extends IStudent {
 }
 const Student = (props: IProps) => {
-  //   console.log(props);
+  // let absents = 0;    Replace it with the line below
+  const [absents, setAbsents] = useState(0);
 
+
+  const addAbsent = () => {
+    // absents += 1;    (JS)Replace it with the line below in React
+    // absents += 1 and assign the absent to the line below? => We can't do like that cause it immutable
+    setAbsents(absents + 1);
+    // setAbsents(absents + 2);
+    // setAbsents(absents + 5); //take the last one
+
+    // setAbsents(oldValue => oldValue + 1);
+    // setAbsents(oldValue => oldValue + 1);
+    // setAbsents(oldValue => oldValue + 1);
+    //In these lines the absent will add 3 times  setState(preValue => updateValue)
+
+    console.log(absents);
+  }
+
+  const removeAbsent = () => {
+    if(absents > 0)
+    setAbsents(absents - 1)
+  }
+
+  const resetAbsent = () => {
+    setAbsents(0);
+  }
   return (
     <div className="std-wrapper">
       <div>
-        <b>Student</b> {props.name + "!"},
+        <b>Student</b> {props.name.toUpperCase() + "!"},
       </div>
       <div>
         <b>Age</b> {props.age},
       </div>
       <div>
         <b>Is Graduated</b> {props.isGraduated ? "Yes" : "No"}
+      </div>
+      <div>
+        <b>Absents:</b> {absents}
+        <button onClick={addAbsent}>+</button>
+        <button onClick={removeAbsent}>-</button>
+        <button onClick={resetAbsent}>Reset</button>
+
       </div>
       <CoursesList list={props.coursesList} />
       <hr />
